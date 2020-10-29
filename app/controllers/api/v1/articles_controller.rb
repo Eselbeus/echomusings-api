b20b4ca21778ba85bc58d60571bfc10017334e57
github username <github email address>
2020-10-28 21:44:19 -0400

    build edit and delete for articles

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::ArticlesController < ApplicationController
+  before_action :load_article, only: [:show, :update, :destroy]
+
   def index
     @articles = Article.all
     render json: @articles
   end
 
   def show
-    @article = Article.find(params[:id])
+    render json: @article
   end
 
   def create
@@ -16,14 +18,21 @@ class Api::V1::ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     @article = Article.update(article_params)
     render json: @article
+  end
+
+  def destroy
+    @article.destroy
   end
 
   private
 
   def article_params
-    params.permit(:user_id, :title, :subtitle, :author, :content, :contentpt2, :contentpt3, :imagelink, :imagelink2, :imagelink3)
+    params.permit(:id, :user_id, :title, :subtitle, :author, :content, :contentpt2, :contentpt3, :imagelink, :imagelink2, :imagelink3)
+  end
+
+  def load_article
+    @article = Article.find(params[:id])
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
   has_many_attached :image
   belongs_to :user
+
+  validates_uniqueness_of :id
 end
